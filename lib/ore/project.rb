b50539fa7dd8ac719270eddac79872ba6481545d
Postmodern <postmodern.mod3@gmail.com>
2010-10-19 23:54:49 -0700

    Added YARD documentation to Ore::Project.

--- lib/ore/project.rb
+++ lib/ore/project.rb
@@ -20,48 +20,75 @@ module Ore
     include Defaults
     include Settings
 
+    # The project metadata file
     METADATA_FILE = 'gem.yml'
 
+    # The SCM which the project is currently under
     attr_reader :scm
 
+    # The files of the project
     attr_reader :project_files
 
+    # The name of the project
     attr_reader :name
 
+    # The version of the project
     attr_reader :version
 
+    # The project summary
     attr_reader :summary
 
+    # The project description
     attr_reader :description
 
+    # The authors of the project
     attr_reader :authors
 
+    # The homepage for the project
     attr_reader :homepage
 
+    # The email contact for the project
     attr_reader :email
 
+    # The build date for any project gems
     attr_reader :date
 
+    # The parsed `.document` file
     attr_reader :document
 
+    # The directories to search within the project when requiring files
     attr_reader :require_paths
 
+    # The names of the executable scripts
     attr_reader :executables
 
+    # The primary executable
     attr_reader :executable
 
+    # Any extra files to include in the project documentation
     attr_reader :extra_files
 
+    # The files of the project
     attr_reader :files
 
+    # The test files for the project
     attr_reader :test_files
 
+    # The dependencies of the project
     attr_reader :dependencies
 
+    # The runtime-dependencies of the project
     attr_reader :runtime_dependencies
 
+    # The development-dependencies of the project
     attr_reader :development_dependencies
 
+    #
+    # Creates a new {Project}.
+    #
+    # @param [String] root
+    #   The root directory of the project.
+    #
     def initialize(root)
       @root = Pathname.new(root).expand_path
 
@@ -187,6 +214,18 @@ module Ore
       end
     end
 
+    #
+    # Finds the project metadata file and creates a new {Project} object.
+    #
+    # @param [String] dir (Dir.pwd)
+    #   The directory to start searching upward from.
+    #
+    # @return [Project]
+    #   The found project.
+    #
+    # @raise [ProjectNotFound]
+    #   No project metadata file could be found.
+    #
     def self.find(dir=Dir.pwd)
       Pathname.new(dir).ascend do |root|
         return self.new(root) if root.join(METADATA_FILE).file?
@@ -195,6 +234,19 @@ module Ore
       raise(ProjectNotFound,"could not find #{METADATA_FILE}")
     end
 
+    #
+    # Executes code within the project.
+    #
+    # @param [String] sub_dir
+    #   An optional sub-directory within the project to execute from.
+    #
+    # @yield []
+    #   The given block will be called once the current working-directory
+    #   has been switched. Once the block finishes executing, the current
+    #   working-directory will be switched back.
+    #
+    # @see http://ruby-doc.org/core/classes/Dir.html#M002314
+    #
     def within(sub_dir=nil,&block)
       dir = if sub_dir
               @root.join(sub_dir)
@@ -205,10 +257,37 @@ module Ore
       Dir.chdir(dir,&block)
     end
 
+    #
+    # Finds paths within the project that match a glob pattern.
+    #
+    # @param [String] pattern
+    #   The glob pattern.
+    #
+    # @yield [path]
+    #   The given block will be passed matching paths.
+    #
+    # @yieldparam [String] path
+    #   A path relative to the root directory of the project.
+    #
     def glob(pattern,&block)
       within { Dir.glob(pattern,&block) }
     end
 
+    #
+    # Populates a Gem Specification using the metadata of the project.
+    #
+    # @yield [gemspec]
+    #   The given block will be passed the populated Gem Specification
+    #   object.
+    #
+    # @yieldparam [Gem::Specification] gemspec
+    #   The newly created Gem Specification.
+    #
+    # @return [Gem::Specification]
+    #   The Gem Specification.
+    #
+    # @see http://rubygems.rubyforge.org/rdoc/Gem/Specification.html
+    #
     def to_gemspec
       gemspec = Gem::Specification.new()
 
@@ -249,6 +328,12 @@ module Ore
       return gemspec
     end
 
+    #
+    # Builds a gem for the project.
+    #
+    # @return [Pathname]
+    #   The path to the built gem file within the `pkg/` directory.
+    #
     def build!
       pkg_dir = @root.join('pkg')
       FileUtils.mkdir_p(pkg_dir)
@@ -262,10 +347,19 @@ module Ore
 
     protected
 
+    #
+    # Prints multiple warning messages.
+    #
+    # @param [Array] messages
+    #   The messages to print.
+    #
     def warn(*messages)
       messages.each { |mesg| STDERR.puts("WARNING: #{mesg}") }
     end
 
+    #
+    # Infers the Source Code Management used by the project.
+    #
     def infer_scm!
       if @root.join('.git').directory?
         @scm = :git
@@ -274,26 +368,67 @@ module Ore
       end
     end
 
+    #
+    # Adds a require-path to the project.
+    #
+    # @param [String] path
+    #   A directory path relative to the project.
+    #
     def add_require_path(path)
       check_directory(path) { |dir| @require_paths << dir }
     end
 
-    def add_executable(path)
+    #
+    # Adds an executable to the project.
+    #
+    # @param [String] name
+    #   The name of the executable.
+    #
+    def add_executable(name)
+      path = File.join('bin',name)
+
       check_executable(path) { |exe| @executables << exe }
     end
 
+    #
+    # Adds an extra-file to the project.
+    #
+    # @param [String] path
+    #   The path to the file, relative to the project.
+    #
     def add_extra_file(path)
       check_file(path) { |file| @extra_files << file }
     end
 
+    #
+    # Adds a file to the project.
+    #
+    # @param [String] path
+    #   The path to the file, relative to the project.
+    #
     def add_file(path)
       check_file(path) { |file| @files << file }
     end
 
+    #
+    # Adds a testing-file to the project.
+    #
+    # @param [String] path
+    #   The path to the testing-file, relative to the project.
+    #
     def add_test_file(path)
       check_file(path) { |file| @test_files << test_file }
     end
 
+    #
+    # Splits a dependencey string.
+    #
+    # @param [String] dep
+    #   The dependencey string.
+    #
+    # @return [Array<String>]
+    #   An Array containing the dependencey name and version.
+    #
     def split_dependencey(dep)
       dep.split(' ',2)
     end
