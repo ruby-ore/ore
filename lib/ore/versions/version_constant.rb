188fffd601ed3f1dac519f2512ff71fc65110b34
Postmodern <postmodern.mod3@gmail.com>
2010-10-20 01:01:48 -0700

    Ore::Versions::VersionConstant.

--- lib/ore/versions/version_constant.rb
+++ lib/ore/versions/version_constant.rb
@@ -2,10 +2,25 @@ require 'ore/versions/version'
 
 module Ore
   module Versions
+    #
+    # Represents a version loaded from a Ruby `VERSION` constant or
+    # `Version` module.
+    #
     class VersionConstant < Version
 
+      # Common file-name that the `VERSION` constant or `Version` module
+      # is defined within.
       FILE_NAME = 'version.rb'
 
+      #
+      # Finds the `version.rb` file.
+      #
+      # @param [String] root
+      #   The root directory of the project.
+      #
+      # @return [VersionConstant, nil]
+      #   The loaded version constant.
+      #
       def self.find(root)
         project_name = File.basename(root)
 
@@ -24,6 +39,16 @@ module Ore
         return nil
       end
 
+      #
+      # Extracts the `VERSION` constant or the major / minor / patch / build
+      # version numbers from the `Version` module.
+      #
+      # @param [String] path
+      #   The path to the `version.rb` file.
+      #
+      # @return [VersionConstant, nil]
+      #   The loaded version constant.
+      #
       def self.load(path)
         major = nil
         minor = nil
@@ -53,12 +78,31 @@ module Ore
 
       protected
 
+      #
+      # Extracts the version string from a `VERSION` constant declaration.
+      #
+      # @param [String] line
+      #   The line of Ruby code where the `VERSION` constant was defined.
+      #
+      # @return [String, nil]
+      #   The extracted version string.
+      #
       def self.extract_string(line)
         if (match = line.match(/=\s*['"](\d+\.\d+\.\d+)['"]/))
           match[1]
         end
       end
 
+      #
+      # Extracts a version number from a `MAJOR`, `MINOR`, `PATCH` or
+      # `BUILD` constant declaration.
+      #
+      # @param [String] line
+      #   The line of Ruby code where the constant was defined.
+      #
+      # @return [Integer, nil]
+      #   The extracted version number.
+      #
       def self.extract_number(line)
         if (match = line.match(/=\s*['"]?(\d+)['"]?/))
           match[1].to_i
