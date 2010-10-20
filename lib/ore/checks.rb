c120ed4c0c000a07b33f5055b926d94ed5f6ea22
Postmodern <postmodern.mod3@gmail.com>
2010-10-19 23:58:14 -0700

    Added documentation to Ore::Checks.

--- lib/ore/checks.rb
+++ lib/ore/checks.rb
@@ -2,6 +2,16 @@ module Ore
   module Checks
     protected
 
+    #
+    # Checks if the path is readable.
+    #
+    # @yield [path]
+    #   The block will be passed the path, if it is readable.
+    #   Otherwise a warning will be printed.
+    #
+    # @yieldparam [String] path
+    #   A readable path.
+    #
     def check_readable(path)
       if File.readable?(path)
         yield path
@@ -10,6 +20,16 @@ module Ore
       end
     end
 
+    #
+    # Checks if the path is a readable directory.
+    #
+    # @yield [path]
+    #   The block will be passed the path, if it is a readable directory.
+    #   Otherwise a warning will be printed.
+    #
+    # @yieldparam [String] path
+    #   The directory path.
+    #
     def check_directory(path)
       check_readable(path) do |dir|
         if File.directory?(dir)
@@ -20,6 +40,16 @@ module Ore
       end
     end
 
+    #
+    # Checks if the path is a readable file.
+    #
+    # @yield [path]
+    #   The block will be passed the path, if it is a readable file.
+    #   Otherwise a warning will be printed.
+    #
+    # @yieldparam [String] path
+    #   A file path.
+    #
     def check_file(path)
       check_readable(path) do |file|
         if File.file?(file)
@@ -30,6 +60,16 @@ module Ore
       end
     end
 
+    #
+    # Checks if the path is an executable file.
+    #
+    # @yield [path]
+    #   The block will be passed the path, if it is an executable file.
+    #   Otherwise a warning will be printed.
+    #
+    # @yieldparam [String] path
+    #   An path to an executable file.
+    #
     def check_executable(path)
       check_file(path) do |file|
         if File.executable?(file)
