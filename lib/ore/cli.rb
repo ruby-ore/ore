082a137e3bf60292847599e08703adb588e162ae
Postmodern <postmodern.mod3@gmail.com>
2010-10-21 21:23:06 -0700

    White space.

--- lib/ore/cli.rb
+++ lib/ore/cli.rb
@@ -8,14 +8,17 @@ module Ore
     default_task :generate
 
     desc 'list', 'List installed Ore templates'
+
     def list
     end
 
     desc 'install URI', 'Installs an Ore template'
+
     def install(uri)
     end
 
     desc 'remove NAME', 'Removes an Ore template'
+
     def remove(name)
     end
 
@@ -28,7 +31,9 @@ module Ore
                             :aliases => '-V'
     method_option :summary, :default => 'TODO: Summary'
     method_option :description, :default => 'TODO: Summary'
+
     def generate(path)
+      puts options.inspect
     end
 
   end
