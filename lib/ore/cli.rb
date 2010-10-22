71d2318fe75d77a5498fdb51547661686d632564
Postmodern <postmodern.mod3@gmail.com>
2010-10-21 22:42:58 -0700

    Added the ore cut task.

--- lib/ore/cli.rb
+++ lib/ore/cli.rb
@@ -5,7 +5,7 @@ require 'thor'
 module Ore
   class CLI < Thor
 
-    default_task :generate
+    default_task :cut
 
     desc 'list', 'List installed Ore templates'
 
@@ -22,18 +22,12 @@ module Ore
     def remove(name)
     end
 
-    desc 'generate PATH', 'Generates a new RubyGem project'
-    method_option :markup, :default => 'rdoc', :aliases => '-m'
-    method_option :templates, :type => :array, :aliases => '-T'
-    method_option :name, :type => :string, :aliases => '-n'
-    method_option :version, :type => :string,
-                            :default => '0.1.0',
-                            :aliases => '-V'
-    method_option :summary, :default => 'TODO: Summary'
-    method_option :description, :default => 'TODO: Summary'
+    desc 'cut', 'Cuts a new RubyGem'
 
-    def generate(path)
-      puts options.inspect
+    def cut
+      require 'ore/project'
+
+      Project.find.build!
     end
 
   end
