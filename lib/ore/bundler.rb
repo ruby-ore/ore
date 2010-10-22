64ce05b6d5f2ac6e38bd820ff8740909933fab5e
Postmodern <postmodern.mod3@gmail.com>
2010-10-22 14:51:27 -0700

    Added 'ore/bundler' file to inject Ore dependencies into Bundler.

---
+++ lib/ore/bundler.rb
@@ -0,0 +1,17 @@
+require 'ore/project'
+
+ore = Ore::Project.find
+
+ore.dependencies.each do |dep|
+  gem(dep.name, *dep.versions)
+end
+
+ore.runtime_dependencies.each do |dep|
+  gem(dep.name, *dep.versions)
+end
+
+group :development do
+  ore.development_dependencies.each do |dep|
+    gem(dep.name, *dep.versions)
+  end
+end
