b3dec68dcdb381623b187d8a20a2036cf1864628
Postmodern <postmodern.mod3@gmail.com>
2010-10-22 01:29:54 -0700

    Filled in CLI#list.

--- lib/ore/cli.rb
+++ lib/ore/cli.rb
@@ -7,9 +7,22 @@ module Ore
 
     default_task :cut
 
+    map '-l' => :list
     desc 'list', 'List installed Ore templates'
 
+    #
+    # Lists builtin and installed templates.
+    #
     def list
+      print_template = lambda { |path|
+        puts "  #{File.basename(path)}"
+      }
+
+      say "Builtin templates:", :green
+      Config.builtin_templates(&print_template)
+
+      say "Installed templates:", :green
+      Config.installed_templates(&print_template)
     end
 
     desc 'install URI', 'Installs an Ore template'
