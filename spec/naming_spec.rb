b45b48988ba1937d2ec676fd2ed57f9b9576c413
Postmodern <postmodern.mod3@gmail.com>
2010-10-23 19:06:26 -0700

    Added specs for Ore::Naming.

---
+++ spec/naming_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'ore/naming'
+
+describe Naming do
+  subject do
+    obj = Object.new
+    obj.extend Ore::Naming
+    obj
+  end
+
+  it "should guess the module names from a project name" do
+    subject.modules_of('foo-bar').should == ['Foo', 'Bar']
+  end
+
+  it "should guess the namespace from a project name" do
+    subject.namespace_of('foo-bar').should == 'Foo::Bar'
+  end
+
+  it "should guess the namespace directory from a project name" do
+    subject.namespace_dir_of('foo-bar').should == 'foo/bar'
+  end
+end
