04e1e8dbda17693ddefe6bb9af8deea1d96aa542
Postmodern <postmodern.mod3@gmail.com>
2010-10-24 17:23:26 -0700

    Use const_missing to automatically load Ore::Specification.
    * Also add ore's lib/ directory back to the $LOAD_PATH to bypass
      Bundler.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,3 +1,27 @@
 module Ore
-  autoload :Specification, 'ore/specification'
+  # The path to the `lib/` directory of ore.
+  ORE_LIB_DIR = File.expand_path(File.dirname(__FILE__))
+
+  #
+  # Provides transparent access to {Ore::Specification}.
+  #
+  # @param [Symbol] name
+  #   The constant name.
+  #
+  # @return [Object]
+  #   The constant.
+  #
+  # @raise [NameError]
+  #   The constant could not be found.
+  #
+  def self.const_missing(name)
+    if name == :Specification
+      $LOAD_PATH << ORE_LIB_DIR unless $LOAD_PATH.include?(ORE_LIB_DIR)
+      require 'ore/specification'
+
+      return Ore.const_get('Specification')
+    end
+
+    super(name)
+  end
 end
