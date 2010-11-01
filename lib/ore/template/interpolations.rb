c6bb22cd8bd9c3be805394241dc16fa33f002518
Patrik Sundberg <patrik@localhost,.(none)>
2010-11-01 22:12:31 +0000

    Replace the :name: convention to be [name] instead. Using : in filenames is not valid on the Windows platform. From http://en.wikipedia.org/wiki/Filename it would seem [ ] is a safe choice.

--- lib/ore/template/interpolations.rb
+++ lib/ore/template/interpolations.rb
@@ -4,10 +4,10 @@ module Ore
     # Handles the expansion of paths and substitution of path keywords.
     # The following keywords are supported:
     #
-    # * `:name:` - The name of the project.
-    # * `:project_dir:` - The directory base-name derived from the project
+    # * `[name]` - The name of the project.
+    # * `[project_dir]` - The directory base-name derived from the project
     #   name.
-    # * `:namespace_dir:` - The full directory path derived from the
+    # * `[namespace_dir]` - The full directory path derived from the
     #   project name.
     #
     module Interpolations
@@ -31,18 +31,18 @@ module Ore
       #   The expanded path.
       #
       # @example Assuming `@project_dir` contains `my_project`.
-      #   interpolate "lib/:project_dir:.rb"
+      #   interpolate "lib/[project_dir].rb"
       #   # => "lib/my_project.rb"
       #
       # @example Assuming `@namespace_dir` contains `my/project`.
-      #   interpolate "spec/:namespace_dir:_spec.rb"
+      #   interpolate "spec/[namespace_dir]_spec.rb"
       #   # => "spec/my/project_spec.rb"
       #
       def interpolate(path)
         dirs = path.split(File::SEPARATOR)
 
         dirs.each do |dir|
-          dir.gsub!(/(:[a-z_]+:)/) do |capture|
+          dir.gsub!(/(\[[a-z_]+\[)/) do |capture|
             keyword = capture[1..-2]
 
             if @@keywords.include?(keyword)
