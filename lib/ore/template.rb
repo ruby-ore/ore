53181619ab78eb2b1e12d13471fcc0798b2e7aeb
Postmodern <postmodern.mod3@gmail.com>
2012-04-27 17:03:04 -0700

    Populate Template.templates in ore/template.rb.

--- lib/ore/template.rb
+++ lib/ore/template.rb
@@ -2,3 +2,10 @@ require 'ore/template/directory'
 require 'ore/template/interpolations'
 require 'ore/template/helpers'
 require 'ore/template/template'
+require 'ore/config'
+
+# register builtin templates
+Ore::Config.builtin_templates { |path| Ore::Template.register(path) }
+
+# register installed templates
+Ore::Config.installed_templates { |path| Ore::Template.register(path) }
